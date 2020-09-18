5f2a7a4de3e113efacffea0a9e57d226e8c7a838
rakshitha-sathyakumar <rakshu595@gmail.com>
2020-09-18 01:24:06 -0700

    add customer signup db connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 
 const login = require("./routes/login");
+const custSignup = require("./routes/customerSignUp");
 
 app.use("/yelp/login", login);
+app.use("/yelp/customerSignUp", custSignup);
 
 const port = process.env.PORT || 3001;
 var server = app.listen(port, () => {

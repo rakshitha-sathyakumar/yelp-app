32f9587bb236a5576fbff946bed464a1101efca0
rakshitha-sathyakumar <rakshu595@gmail.com>
2020-09-18 12:51:50 -0700

    add restaurant signup

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,12 @@ const app = require('./app');
 
 const login = require("./routes/login");
 const custSignup = require("./routes/customerSignUp");
+const restSignup = require("./routes/restSignUp");
 
 app.use("/yelp/login", login);
 app.use("/yelp/customerSignUp", custSignup);
+app.use("/yelp/restSignUp", restSignup);
+
 
 const port = process.env.PORT || 3001;
 var server = app.listen(port, () => {
---
+++ Backend/routes/restSignUp.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+// const passwordHash = require('password-hash');
+const pool = require('../pool.js');
+
+router.post('/', (req, res) => {
+    let sql = `CALL add_restaurant('${req.body.name}', '${req.body.email}', '${req.body.password}', '${req.body.address}', '${req.body.zipcode}');`;
+    console.log(sql);
+    pool.query(sql, (err, result) => {
+        console.log(err);
+      if (err) {
+        // res.writeHead(500, {
+        //   'Content-Type': 'text/plain'
+        // });
+        res.end("Error in Data");
+      }
+      if (result && result.length > 0 && result[0][0].status === 'RESTAURANT_ADDED') {
+        // res.writeHead(200, {
+        //   'Content-Type': 'text/plain'
+        // })
+        res.end(result[0][0].status);
+      }
+      else if (result && result.length > 0 && result[0][0].status === 'RESTAURANT_EXISTS') {
+        // res.writeHead(401, {
+        //   'Content-Type': 'text/plain'
+        // })
+        res.end(result[0][0].status);
+      }
+    });
+  });
+
+  module.exports = router;
